ccdb6628de00ccd8db0400b9080640a120f850aa
Alexey Kuznetsov <axet@me.com>
2016-07-19 13:02:44 +0300

    update build script

--- libtorrent/release.sh
+++ libtorrent/release.sh
@@ -3,6 +3,8 @@
 # https://groups.google.com/forum/#!topic/go-mobile/ZstjAiIFrWY
 #
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 go get -u github.com/axet/libtorrent || exit 1
 
-gomobile bind -o libtorrent.aar -ldflags "-s" github.com/axet/libtorrent || exit 1
+gomobile bind -o "$DIR/libtorrent.aar" -ldflags "-s" github.com/axet/libtorrent || exit 1
