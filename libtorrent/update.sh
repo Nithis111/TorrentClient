7943d2fc7e04c160da22a397dd84e7efc18d1eb2
Alexey Kuznetsov <axet@me.com>
2016-07-23 18:12:43 +0300

    drop submodules

--- libtorrent/update.sh
+++ libtorrent/update.sh
@@ -4,4 +4,21 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 GOPATH=$DIR
 
+go_get() {
+  F="$1"
+  T="$2"
+  
+  TT="$DIR/src/$T"
+  
+  if [ ! -e "$TT" ]; then
+    git clone "https://$F" "$TT" || return 1
+  fi
+  
+  return 0
+}
+
+go_get "github.com/axet/torrent" "github.com/anacrolix/torrent" || exit 1
+
 go get -u github.com/axet/libtorrent || exit 1
+
+go get -u golang.org/x/mobile/cmd/gomobile || exit 1
