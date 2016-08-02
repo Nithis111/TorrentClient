b6b77e52c9cfe7c3927c859794d58b8791a4fc05
Alexey Kuznetsov <axet@me.com>
2016-08-02 15:47:40 +0300

    more log message

--- app/src/main/java/com/github/axet/torrentclient/dialogs/BrowserDialogFragment.java
+++ app/src/main/java/com/github/axet/torrentclient/dialogs/BrowserDialogFragment.java
@@ -9,14 +9,19 @@ import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.ConsoleMessage;
 import android.webkit.DownloadListener;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
 import android.webkit.WebResourceError;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebResourceResponse;
@@ -107,6 +112,14 @@ public class BrowserDialogFragment extends DialogFragment implements MainActivit
 
         web = (WebView) v.findViewById(R.id.webview);
 
+        web.getSettings().setSupportMultipleWindows(true);
+        web.getSettings().setDomStorageEnabled(true);
+        web.getSettings().setJavaScriptEnabled(true);
+        web.getSettings().setLoadWithOverviewMode(true);
+        web.getSettings().setUseWideViewPort(true);
+        web.getSettings().setBuiltInZoomControls(true);
+        web.getSettings().setDisplayZoomControls(true);
+
         back = (ImageButton) v.findViewById(R.id.search_details_back);
         forward = (ImageButton) v.findViewById(R.id.search_details_forward);
         back.setOnClickListener(new View.OnClickListener() {
@@ -126,11 +139,48 @@ public class BrowserDialogFragment extends DialogFragment implements MainActivit
 
         String url = getArguments().getString("url");
 
+        web.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+            }
+
+            @Override
+            public boolean onJsTimeout() {
+                Log.d(TAG, "onJsTimeout");
+                return super.onJsTimeout();
+            }
+
+            @Override
+            public boolean onCreateWindow(WebView view, boolean isDialog, boolean isUserGesture, Message resultMsg) {
+                return true;
+            }
+
+            @Override
+            public boolean onConsoleMessage(final ConsoleMessage consoleMessage) {
+                String msg = consoleMessage.message() + " " + consoleMessage.lineNumber();
+                onConsoleMessage(msg, 0, "");
+                return true;//super.onConsoleMessage(consoleMessage);
+            }
+
+            @Override
+            public void onConsoleMessage(String msg, int lineNumber, String sourceID) {
+                Log.d(TAG, msg);
+            }
+
+            @Override
+            public boolean onJsAlert(WebView view, String url, final String message, JsResult result) {
+                getMainActivity().post(message);
+                result.confirm();
+                return true;//super.onJsAlert(view, url, message, result);
+            }
+
+        });
+
         web.setWebViewClient(new WebViewClient() {
             @Override
             public void onPageFinished(WebView view, String url) {
                 super.onPageFinished(view, url);
-
                 updateButtons();
             }
 
@@ -191,6 +241,7 @@ public class BrowserDialogFragment extends DialogFragment implements MainActivit
         web.setDownloadListener(new DownloadListener() {
             @Override
             public void onDownloadStart(final String url, String userAgent, String contentDisposition, String mimetype, long contentLength) {
+                Log.d(TAG, "onDownloadStart " + url);
                 thread = new Thread(new Runnable() {
                     @Override
                     public void run() {
@@ -210,12 +261,6 @@ public class BrowserDialogFragment extends DialogFragment implements MainActivit
                 thread.start();
             }
         });
-
-        web.getSettings().setJavaScriptEnabled(true);
-        web.getSettings().setLoadWithOverviewMode(true);
-        web.getSettings().setUseWideViewPort(true);
-        web.getSettings().setBuiltInZoomControls(true);
-        web.getSettings().setDisplayZoomControls(true);
 
         web.loadUrl(url);
 
