9be7313e6a50b7372e0992178a019403c562ab9a
Alexey Kuznetsov <axet@me.com>
2016-11-20 20:15:28 +0300

    fix readonly storage

---
+++ app/src/main/java/com/github/axet/torrentclient/navigators/Crawl.java
@@ -0,0 +1,56 @@
+package com.github.axet.torrentclient.navigators;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
+
+import com.github.axet.torrentclient.activities.MainActivity;
+
+public class Crawl extends Search {
+    public static final String TAG = Crawl.class.getSimpleName();
+
+    private static final String TEXT_TYPE = " TEXT";
+    private static final String COMMA_SEP = ",";
+    private static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + FeedEntry.TABLE_NAME + " (" +
+                    FeedEntry._ID + " INTEGER PRIMARY KEY," +
+                    FeedEntry.COLUMN_NAME_TITLE + TEXT_TYPE + COMMA_SEP +
+                    FeedEntry.COLUMN_NAME_SUBTITLE + TEXT_TYPE + " )";
+
+    private static final String SQL_DELETE_ENTRIES =
+            "DROP TABLE IF EXISTS " + FeedEntry.TABLE_NAME;
+
+    public static class FeedEntry implements BaseColumns {
+        public static final String TABLE_NAME = "crawl";
+        public static final String COLUMN_NAME_TITLE = "title";
+        public static final String COLUMN_NAME_SUBTITLE = "subtitle";
+    }
+
+    public static class CrawlDbHelper extends SQLiteOpenHelper {
+        // If you change the database schema, you must increment the database version.
+        public static final int DATABASE_VERSION = 1;
+        public static final String DATABASE_NAME = "crawl.db";
+
+        public CrawlDbHelper(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL(SQL_CREATE_ENTRIES);
+        }
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            // This database is only a cache for online data, so its upgrade policy is
+            // to simply to discard the data and start over
+            db.execSQL(SQL_DELETE_ENTRIES);
+            onCreate(db);
+        }
+        public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            onUpgrade(db, oldVersion, newVersion);
+        }
+    }
+
+    public Crawl(MainActivity m) {
+        super(m);
+    }
+
+}
