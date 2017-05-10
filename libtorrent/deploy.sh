2cdf6a2270b612b818dfe2d4285a4f6b0feba072
Alexey Kuznetsov <axet@me.com>
2017-05-10 14:52:20 +0300

    drop libtorrent build scrtips. move them to libtorrent repo

--- libtorrent/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mvn gpg:sign-and-deploy-file -DpomFile=pom.xml -Dfile=libtorrent.aar \
-  -DrepositoryId=sonatype-nexus-staging \
-  -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/
-
-mvn install:install-file -DpomFile=pom.xml -Dfile=libtorrent.aar \
-  -DrepositoryId=sonatype-nexus-staging \
-  -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/
