1b1ae86c423302a6bcb129e100560427e5ab7c48
Alexey Kuznetsov <axet@me.com>
2017-05-12 06:58:17 +0300

    fix thread

--- app/src/main/java/com/github/axet/torrentclient/activities/BootActivity.java
+++ app/src/main/java/com/github/axet/torrentclient/activities/BootActivity.java
@@ -4,26 +4,11 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.github.axet.torrentclient.app.MainApplication;
 
 public class BootActivity extends Activity {
-    public MainApplication getApp() {
-        return (MainApplication) getApplication();
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        getApp().create();
-
-        if (android.os.Build.VERSION.SDK_INT >= 21) {
-            finishAndRemoveTask();
-        } else {
-            finish();
-        }
-    }
 
     public static void createApplication(Context context) {
         Intent intent = new Intent(context, BootActivity.class);
@@ -31,5 +16,32 @@ public class BootActivity extends Activity {
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NO_ANIMATION | Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
 
         context.startActivity(intent);
+    }
+
+    Thread initThread;
+    Handler handler = new Handler();
+
+    public MainApplication getApp() {
+        return (MainApplication) getApplication();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                getApp().create();
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (isFinishing())
+                            return;
+                        moveTaskToBack(true);
+                    }
+                });
+            }
+        });
+        initThread.start();
     }
 }
