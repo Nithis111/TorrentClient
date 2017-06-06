72d59a9e0ead2477ef13040558b5228670de17d5
Alexey Kuznetsov <axet@me.com>
2017-06-06 19:32:45 +0300

    enable crawl loader

--- app/src/main/java/com/github/axet/torrentclient/navigators/Crawl.java
+++ app/src/main/java/com/github/axet/torrentclient/navigators/Crawl.java
@@ -1,10 +1,14 @@
 package com.github.axet.torrentclient.navigators;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteQueryBuilder;
 import android.provider.BaseColumns;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.widget.AbsListView;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
@@ -16,6 +20,7 @@ import com.github.axet.androidlibrary.widgets.HeaderGridView;
 import com.github.axet.androidlibrary.widgets.ThemeUtils;
 import com.github.axet.torrentclient.R;
 import com.github.axet.torrentclient.activities.MainActivity;
+import com.github.axet.torrentclient.app.SearchEngine;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -23,8 +28,13 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
+import java.util.Locale;
 import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Crawl extends Search {
     public static final String TAG = Crawl.class.getSimpleName();
@@ -32,23 +42,37 @@ public class Crawl extends Search {
     private static final String TEXT_TYPE = " TEXT";
     private static final String COMMA_SEP = ",";
     private static final String SQL_CREATE_ENTRIES =
-            "CREATE TABLE " + FeedEntry.TABLE_NAME + " (" +
-                    FeedEntry._ID + " INTEGER PRIMARY KEY," +
-                    FeedEntry.COLUMN_ENGINE + TEXT_TYPE + COMMA_SEP +
-                    FeedEntry.COLUMN_TITLE + TEXT_TYPE + COMMA_SEP +
-                    FeedEntry.COLUMN_DETAILS + TEXT_TYPE + COMMA_SEP +
-                    FeedEntry.COLUMN_MAGNET + TEXT_TYPE + COMMA_SEP +
-                    FeedEntry.COLUMN_TORRENT + TEXT_TYPE + COMMA_SEP +
-                    FeedEntry.COLUMN_DATE + TEXT_TYPE +
+            "CREATE TABLE " + CrawlEntry.TABLE_NAME + " (" +
+                    CrawlEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT" + COMMA_SEP +
+                    CrawlEntry.COLUMN_ENGINE + TEXT_TYPE + COMMA_SEP +
+                    CrawlEntry.COLUMN_TITLE + TEXT_TYPE + COMMA_SEP +
+                    CrawlEntry.COLUMN_IMAGE + TEXT_TYPE + COMMA_SEP +
+                    CrawlEntry.COLUMN_DETAILS + TEXT_TYPE + COMMA_SEP +
+                    CrawlEntry.COLUMN_MAGNET + TEXT_TYPE + COMMA_SEP +
+                    CrawlEntry.COLUMN_TORRENT + TEXT_TYPE + COMMA_SEP +
+                    CrawlEntry.COLUMN_DATE + TEXT_TYPE +
                     " )";
 
-    private static final String SQL_DELETE_ENTRIES =
-            "DROP TABLE IF EXISTS " + FeedEntry.TABLE_NAME;
+    private static final String SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS " + CrawlEntry.TABLE_NAME;
 
-    public static class FeedEntry implements BaseColumns {
+    public static boolean isString(String s) {
+        if (s.length() < 2)
+            return false;
+        char[] chars = s.toCharArray();
+        for (char c : chars) {
+            if (Character.isLetter(c))
+                return true;
+            if (Character.isDigit(c) && c != '-')
+                return true;
+        }
+        return true;
+    }
+
+    public static class CrawlEntry implements BaseColumns {
         public static final String TABLE_NAME = "crawl";
         public static final String COLUMN_ENGINE = "engine";
         public static final String COLUMN_TITLE = "title";
+        public static final String COLUMN_IMAGE = "image";
         public static final String COLUMN_DETAILS = "details";
         public static final String COLUMN_MAGNET = "magnet";
         public static final String COLUMN_TORRENT = "torrent";
@@ -60,11 +84,14 @@ public class Crawl extends Search {
         public static final int DATABASE_VERSION = 1;
         public static final String DATABASE_NAME = "crawl.db";
 
+        private SQLiteDatabase mDatabase;
+
         public CrawlDbHelper(Context context) {
             super(context, DATABASE_NAME, null, DATABASE_VERSION);
         }
 
         public void onCreate(SQLiteDatabase db) {
+            this.mDatabase = db;
             db.execSQL(SQL_CREATE_ENTRIES);
         }
 
@@ -78,6 +105,130 @@ public class Crawl extends Search {
         public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
             onUpgrade(db, oldVersion, newVersion);
         }
+
+        public long addCrawl(String engine, String title, String image, String details, String date) {
+            SQLiteDatabase db = getWritableDatabase();
+            ContentValues initialValues = new ContentValues();
+            initialValues.put(CrawlEntry.COLUMN_ENGINE, engine);
+            initialValues.put(CrawlEntry.COLUMN_TITLE, title);
+            initialValues.put(CrawlEntry.COLUMN_IMAGE, image);
+            initialValues.put(CrawlEntry.COLUMN_DETAILS, details);
+            initialValues.put(CrawlEntry.COLUMN_MAGNET, "");
+            initialValues.put(CrawlEntry.COLUMN_TORRENT, "");
+            initialValues.put(CrawlEntry.COLUMN_DATE, date);
+            return db.insert(CrawlEntry.TABLE_NAME, null, initialValues);
+        }
+
+        public SearchItem get(long id) {
+            String selection = CrawlEntry._ID + " = ?";
+            String[] selectionArgs = new String[]{"" + id};
+            Cursor c = query(selection, selectionArgs, null);
+            if (c == null)
+                return null;
+            SearchItem s = new SearchItem();
+            s.title = getString(c, CrawlEntry.COLUMN_TITLE);
+            s.image = getString(c, CrawlEntry.COLUMN_IMAGE);
+            s.details = getString(c, CrawlEntry.COLUMN_DETAILS);
+            s.date = getString(c, CrawlEntry.COLUMN_DATE);
+            return s;
+        }
+
+        String getString(Cursor c, String name) {
+            int i = c.getColumnIndex(name);
+            if (i == -1)
+                return null;
+            return c.getString(i);
+        }
+
+        public Cursor exist(String title) {
+            String selection = CrawlEntry.COLUMN_TITLE + " = ?";
+            String[] selectionArgs = new String[]{title};
+            Cursor c = query(selection, selectionArgs, null);
+            return c;
+        }
+
+        private Cursor query(String selection, String[] selectionArgs, String[] columns) {
+            SQLiteQueryBuilder builder = new SQLiteQueryBuilder();
+            builder.setTables(CrawlEntry.TABLE_NAME);
+
+            Cursor cursor = builder.query(getReadableDatabase(),
+                    columns, selection, selectionArgs, null, null, null);
+
+            if (cursor == null) {
+                return null;
+            } else if (!cursor.moveToFirst()) {
+                cursor.close();
+                return null;
+            }
+            return cursor;
+        }
+    }
+
+    public static class CrawlDbIndexHelper extends SQLiteOpenHelper {
+        public static final int DATABASE_VERSION = 1;
+        public static final String FTS_VIRTUAL_TABLE = "FTS";
+        public static final String DATABASE_NAME = "crawl-index.db";
+
+        public static final String COL_WORD = "WORD";
+        public static final String COL_CRAWL = "CRAWL_ID";
+
+        private final Context mHelperContext;
+        private SQLiteDatabase mDatabase;
+
+        public static final String FTS_TABLE_CREATE =
+                "CREATE VIRTUAL TABLE " + FTS_VIRTUAL_TABLE +
+                        " USING fts3 (" +
+                        COL_WORD + ", " +
+                        COL_CRAWL + ")";
+
+        CrawlDbIndexHelper(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+            mHelperContext = context;
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            mDatabase = db;
+            mDatabase.execSQL(FTS_TABLE_CREATE);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            Log.w(TAG, "Upgrading database from version " + oldVersion + " to "
+                    + newVersion + ", which will destroy all old data");
+            db.execSQL("DROP TABLE IF EXISTS " + FTS_VIRTUAL_TABLE);
+            onCreate(db);
+        }
+
+        public long addWord(String word, long definition) {
+            SQLiteDatabase db = getWritableDatabase();
+            ContentValues initialValues = new ContentValues();
+            initialValues.put(COL_WORD, word);
+            initialValues.put(COL_CRAWL, definition);
+            return db.insert(FTS_VIRTUAL_TABLE, null, initialValues);
+        }
+
+        public Cursor getWordMatches(String query, String[] columns) {
+            String selection = COL_WORD + " MATCH ?";
+            String[] selectionArgs = new String[]{query + "*"};
+            return query(selection, selectionArgs, columns);
+        }
+
+        private Cursor query(String selection, String[] selectionArgs, String[] columns) {
+            SQLiteQueryBuilder builder = new SQLiteQueryBuilder();
+            builder.setTables(FTS_VIRTUAL_TABLE);
+
+            Cursor cursor = builder.query(getReadableDatabase(),
+                    columns, selection, selectionArgs, null, null, null);
+
+            if (cursor == null) {
+                return null;
+            } else if (!cursor.moveToFirst()) {
+                cursor.close();
+                return null;
+            }
+            return cursor;
+        }
     }
 
     public static class State {
@@ -88,6 +239,35 @@ public class Crawl extends Search {
         public String url;
         public Map<String, String> s; // json 'crawl'
         public String next;
+
+        public JSONObject save() {
+            try {
+                JSONObject o = new JSONObject();
+                o.put("name", name);
+                o.put("page", page);
+                o.put("end", end);
+                o.put("last", last);
+                o.put("url", url);
+                o.put("next", next);
+                return o;
+            } catch (JSONException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        public void load(String state) {
+            try {
+                JSONObject o = new JSONObject(state);
+                name = o.getString("name");
+                page = o.getInt("page");
+                end = o.getBoolean("end");
+                last = o.getLong("last");
+                url = o.getString("url");
+                next = o.optString("next", null);
+            } catch (JSONException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
     FrameLayout progressFrame;
@@ -109,8 +289,13 @@ public class Crawl extends Search {
     };
     Thread thread;
 
+    CrawlDbHelper db;
+    CrawlDbIndexHelper index;
+
     public Crawl(MainActivity m) {
         super(m);
+        db = new CrawlDbHelper(m);
+        index = new CrawlDbIndexHelper(m);
     }
 
     public void install(final HeaderGridView list) {
@@ -136,6 +321,10 @@ public class Crawl extends Search {
             s.s = engine.getMap("crawl");
             this.crawls.put(key, s);
         }
+        for (String key : new TreeSet<>(this.crawls.keySet())) { // drop old engines
+            if (!crawls.containsKey(key))
+                this.crawls.remove(key);
+        }
 
         crawlNextThread();
     }
@@ -154,7 +343,6 @@ public class Crawl extends Search {
             @Override
             public void run() {
                 crawlNext();
-                thread = null;
             }
         });
         thread.start();
@@ -166,6 +354,12 @@ public class Crawl extends Search {
             JSONObject json = new JSONObject();
             String state = super.save();
             json.put("state", state);
+            Map<String, String> map = new TreeMap<>();
+            for (String k : crawls.keySet()) {
+                State s = crawls.get(k);
+                map.put(k, s.save().toString());
+            }
+            json.put("crawls", SearchEngine.toJSON(map));
             return json.toString();
         } catch (JSONException e) {
             throw new RuntimeException(e);
@@ -176,6 +370,18 @@ public class Crawl extends Search {
     public void load(String state) {
         try {
             JSONObject json = new JSONObject(state);
+            JSONObject cc = json.optJSONObject("crawls");
+            if (cc != null) {
+                Map<String, Object> c = SearchEngine.toMap(cc);
+                for (String k : c.keySet()) {
+                    String ss = (String) c.get(k);
+                    State s = crawls.get(k);
+                    if (s == null)
+                        s = new State();
+                    s.load(ss);
+                    crawls.put(k, s);
+                }
+            }
             String s = json.getString("state");
             super.load(s);
         } catch (JSONException e) {
@@ -185,7 +391,7 @@ public class Crawl extends Search {
 
     void crawlLoad(State state, Runnable done) {
         String url = state.next;
-        if (url == null)
+        if (url == null || url.isEmpty())
             url = state.url;
         HttpClient.DownloadResponse html = null;
         if (state.s.containsKey("get")) {
@@ -211,10 +417,35 @@ public class Crawl extends Search {
         Elements list = doc.select(select);
         for (int i = 0; i < list.size(); i++) {
             SearchItem item = searchItem(state.s, url, list.get(i).outerHtml());
+            Cursor c = db.exist(item.title);
+            if (state.end) {
+                if (c != null) {
+                    state.next = null;
+                    c.close();
+                    return;
+                }
+            } else {
+                if (c != null) {
+                    dropCrawl(c.getLong(0));
+                    c.close();
+                }
+            }
+            long id = db.addCrawl(state.name, item.title, item.image, item.details, item.date);
+            String[] ss = item.title.split("\\s+");
+            Locale en = new Locale("en");
+            for (String s : ss) {
+                s = s.trim();
+                s = s.toLowerCase(en);
+                if (isString(s))
+                    index.addWord(s, id);
+            }
             Log.d(TAG, "item " + item.title);
         }
 
         String next = matcher(url, html, state.s.get("next"));
+        if (next == null) {
+            state.end = true;
+        }
 
         state.page++;
         state.next = next;
@@ -238,10 +469,10 @@ public class Crawl extends Search {
             thread.interrupt();
             try {
                 thread.join();
+                thread = null;
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
             }
-            thread = null;
         }
         handler.removeCallbacks(crawlDelay);
         handler.removeCallbacks(crawlNext);
@@ -251,5 +482,48 @@ public class Crawl extends Search {
     public void close() {
         super.close();
         stop();
+    }
+
+    @Override
+    public void search(final Map<String, String> s, final String search, final Runnable done) {
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                searchUI(s, search, done);
+            }
+        });
+    }
+
+    void searchUI(Map<String, String> s, String search, final Runnable done) {
+        String select = null;
+        String l = s.get("list");
+        if (l != null) {
+            select = l;
+            gridView = null;
+        }
+        String g = s.get("grid");
+        if (g != null) {
+            select = g;
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            gridView = inflater.inflate(R.layout.search_item_grid, grid, false);
+        }
+        gridUpdate();
+
+        this.list.clear();
+        Cursor c = index.getWordMatches(search, null);
+        if (c == null)
+            return;
+        while (c.moveToNext()) {
+            long id = c.getLong(1);
+            SearchItem item = db.get(id);
+            this.list.add(item);
+        }
+        c.close();
+        notifyDataSetChanged();
+    }
+
+    void dropCrawl(long id) {
+        db.getWritableDatabase().delete(CrawlEntry.TABLE_NAME, CrawlEntry._ID + " == ?", new String[]{"" + id});
+        index.getWritableDatabase().delete(CrawlDbIndexHelper.FTS_VIRTUAL_TABLE, CrawlDbIndexHelper.COL_CRAWL + " == ?", new String[]{"" + id});
     }
 }
